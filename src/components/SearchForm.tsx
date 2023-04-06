c99744f0b43b9210ebd9af0c0541ed3905c5c9cb
田中光 <tanakahikari@M0176-MacBook-Pro.local>
2023-04-06 19:39:49 +0900

    WIP: context-SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,17 +1,17 @@
 import { Button, Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { getRepositories } from "./getQueries";
-import { QueryContext, searchFormContext } from "../contexts";
+import { searchFormContext } from "../contexts";
 import { useContext } from "react";
 
 const SearchForm = (props: any) => {
-  const { setRepositories } = useContext(searchFormContext);
-  const { setEndCursor } = useContext(searchFormContext);
-  const { setQuery } = useContext(searchFormContext);
-  const { setHasMoreItems } = useContext(searchFormContext);
+  const { setRepositories, setEndCursor, setQuery, setHasMoreItems } =
+    useContext(searchFormContext);
+  const params = useContext(searchFormContext);
+  const query = params.query;
+  const endCursor = params.endCursor;
+  const repositories = params.repositories;
 
-  const { endCursor, repositories } = props;
-  const query = useContext(QueryContext);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setRepositories([]);
--- src/contexts.ts
+++ src/contexts.ts
@@ -10,6 +10,9 @@ type getRepositoriesValue = {
   repositories: Repository[];
 };
 type SearchForm = {
+  query: string;
+  endCursor: string | null;
+  repositories: Repository[];
   setRepositories: (repositories: Repository[]) => void;
   setEndCursor: (endCursor: string | null) => void;
   setHasMoreItems: (hasMoreItems: boolean) => void;
@@ -23,6 +26,9 @@ export const getRepositoriesContext = createContext<getRepositoriesValue>({
   repositories: [],
 });
 export const searchFormContext = createContext<SearchForm>({
+  query: "",
+  endCursor: "",
+  repositories: [],
   setRepositories: () => {},
   setEndCursor: () => {},
   setHasMoreItems: () => {},
